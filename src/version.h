00ae0de3fc42864e5ce79d28eb538791dd617b23
Mihai Donțu <mihai.dontu@gmail.com>
2026-01-03 21:07:07 +0200

    fix the version display

--- src/version.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-#pragma once
-
-constexpr auto FUSE_SPECTRUM_VERSION_MAJOR = 1u;
-constexpr auto FUSE_SPECTRUM_VERSION_MINOR = 0u;
-constexpr auto FUSE_SPECTRUM_VERSION_PATCH = 0u;
